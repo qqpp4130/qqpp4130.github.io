026cdd7ce48fad7126d865ca90b5efed39c21c72
Raymond <36610776+qqpp4130@users.noreply.github.com>
2023-12-15 07:04:57 -0700

    Update booking.js
    Booking js completed

--- scripts/booking.js
+++ scripts/booking.js
@@ -2,51 +2,67 @@
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-let dayContainer = document.getElementByClassName("day-selector");
-let days = dayContainer.getElementsByClassName("blue-holder");
+const dayContainer = document.querySelectorAll(".day-selector li");
 let dayCounter = 0;
-const dayCost = 10;
-const halfCost = 7;
+const clearButton = document.querySelector("#clear-button");
+const fullDayButton = document.querySelector("#full");
+const halfDayButton = document.querySelector("#half");
+const fullCost = 35;
+const halfCost = 20;
+let rate = fullCost;
 let cost = 0;
-
+function updateCost() {
+    cost = dayCounter * rate;
+    document.querySelector("#calculated-cost").innerHTML = cost;
+};
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-for (var i = 0; i < days.length; i++) {
-    days[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("clicked");
-        if (current.length > 0) {
-        current[0].className = current[0].className.replace(" clicked", "");
+for (var i = 0; i < dayContainer.length; i++) {
+    dayContainer[i].addEventListener("click", function() {
+        if (this.classList.contains("clicked")) {
+            this.classList.remove("clicked");;
+        } else {
+            this.classList.add("clicked");
         }
-        this.className += " clicked";
+        dayCounter = document.querySelectorAll(".day-selector li.clicked").length;
+        updateCost()
     });
 }
-
-
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
-
-
-
-
+clearButton.addEventListener("click", function() {
+    for (var i = 0; i < dayContainer.length; i++) {
+        dayContainer[i].classList.remove("clicked");
+    }
+    dayCounter = 0;
+    updateCost()
+});
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-
-
+halfDayButton.addEventListener("click", function() {
+    rate = halfCost;
+    this.classList.add("clicked");
+    fullDayButton.classList.remove("clicked");
+    updateCost()
+})
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
-
-
-
+fullDayButton.addEventListener("click", function() {
+    rate = fullCost;
+    this.classList.add("clicked");
+    halfDayButton.classList.remove("clicked");
+    updateCost()
+})
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 
-
+// up in dayContainer event listener.
